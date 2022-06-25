8a64c6ef3bc4c39a6d68409c82007e09046bc5a5
贾海亮 <www.51candy@gmail.com>
2022-06-25 09:42:48 +0800

    添加 UItests

---
+++ HLCandyTests/HLCandyTests.m
@@ -0,0 +1,37 @@
+//
+//  HLCandyTests.m
+//  HLCandyTests
+//
+//  Created by 贾海亮 on 2022/6/25.
+//  Copyright © 2022 seabright. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HLCandyTests : XCTestCase
+
+@end
+
+@implementation HLCandyTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ HLCandyUITests/HLCandyUITests.m
@@ -0,0 +1,48 @@
+//
+//  HLCandyUITests.m
+//  HLCandyUITests
+//
+//  Created by 贾海亮 on 2022/6/25.
+//  Copyright © 2022 seabright. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HLCandyUITests : XCTestCase
+
+@end
+
+@implementation HLCandyUITests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // UI tests must launch the application that they test.
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testLaunchPerformance {
+    if (@available(macOS 10.15, iOS 13.0, tvOS 13.0, *)) {
+        // This measures how long it takes to launch your application.
+        [self measureWithMetrics:@[XCTOSSignpostMetric.applicationLaunchMetric] block:^{
+            [[[XCUIApplication alloc] init] launch];
+        }];
+    }
+}
+
+@end
